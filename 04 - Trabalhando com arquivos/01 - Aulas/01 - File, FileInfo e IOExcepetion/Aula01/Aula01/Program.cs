33999ac0ee8bb7edc12e85f01483ed9427d6a894
Lucas-Casarotti <casarotti@hotmail.com>
2024-11-18 20:49:55 -0300

    v5 - Trabalhando com arquivos

---
+++ 04 - Trabalhando com arquivos/01 - Aulas/01 - File, FileInfo e IOExcepetion/Aula01/Aula01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Aula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"E:\Usuários\Lucas\Área de Trabalho\file1.txt";
+            string targetPath = @"E:\Usuários\Lucas\Área de Trabalho\file2.txt";
+
+            try
+            {
+                // Utilizando o File
+                File.Copy(sourcePath, targetPath);
+
+
+                // Utilizando o FileInfo
+                // Instanciando um objeto do tipo FileInfo, associado ao arquivo da variável sourcePath
+                //FileInfo fileInfo = new FileInfo(sourcePath);
+                // Copiando o conteúdo do file1 para o file2
+                //fileInfo.CopyTo(targetPath);
+
+
+
+                // Qual a diferença?
+
+                //File é uma classe estática, então seus métodos podem ser usados diretamente sem precisar criar um objeto.
+                //Isso é conveniente para operações simples e rápidas de arquivos, sem a necessidade de instanciar uma classe.
+
+                // FileInfo é uma classe que exige a criação de uma instância associada a um arquivo específico no sistema.
+                // Depois que uma instância é criada, ela mantém informações sobre o arquivo, como seu nome, diretório
+                // e data de modificação, permitindo que o código reutilize essas informações sem ter que acessar
+                // o sistema de arquivos repetidamente.
+
+            }
+            catch (IOException e) 
+            {
+                Console.WriteLine("Ocorreu um erro!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ 04 - Trabalhando com arquivos/01 - Aulas/02 - FileStream e StreamReader/Aula02/Aula02/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Aula02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Usuários\Lucas\Área de Trabalho\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+
+                // Lê o arquivo inteiro e retorna uma string
+                string line = sr.ReadToEnd();
+
+                // Lê a primeira linha completa
+                //string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Erro!");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // É necessário fechar, pois enquanto um FileStream ou StreamReader está aberto,
+                // ele mantém um "bloqueio" (lock) sobre o arquivo, impedindo que outros processos o acessem. 
+                // Fechar a stream libera o arquivo para que outras operações possam ser executadas.
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+
+            // Maneira mais resumida, utilizando a classe File
+
+            //string path2 = @"E:\Usuários\Lucas\Área de Trabalho\file1.txt";
+            //StreamReader sr2 = null;
+            //try
+            //{
+            //    // Já instancia o FileStream
+            //    sr2 = File.OpenText(path2);
+
+            //    string line = sr2.ReadLine();
+            //    Console.WriteLine(line);
+            //}
+            //catch (IOException ex)
+            //{
+            //    Console.WriteLine("Erro!");
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    if (sr != null) sr.Close();
+            //}
+        }
+    }
+}
