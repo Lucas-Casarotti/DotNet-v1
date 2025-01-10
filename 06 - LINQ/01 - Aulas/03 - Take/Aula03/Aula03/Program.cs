2b2e4e816a939e1311238256a1eb28f958b7e174
Lucas-Casarotti <casarotti@hotmail.com>
2025-01-10 09:55:13 -0300

    v7 - LINQ

---
+++ 06 - LINQ/01 - Aulas/03 - Take/Aula03/Aula03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Aula03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Utilizando Take para pegar uma parte de uma coleção
+            var numeros = new int[] { 5, 4, 3, 2, 1 };
+
+            var resultado = numeros.Take(3);
+
+            foreach (int numero in resultado)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.WriteLine("");
+
+            // Ordenando os valores pegos pelo Take
+            // Também é possível adicionar uma condição
+            var resultadoOrdenado = numeros.Where(p => p <4).Take(3).OrderBy(x => x);
+
+            foreach (int numero in resultadoOrdenado)
+            {
+                Console.WriteLine(numero);
+            }
+
+        }
+    }
+}
---
+++ 06 - LINQ/01 - Aulas/04 - Count/Aula04/Aula04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Aula04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numeros = new int[] {1, 2, 3, 4, 5};
+
+            // Utilizando o Count para contar a quantidade de elementos
+            // Também é possível adicionar condições
+            var resultado = numeros.Count(p => p > 2);
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
