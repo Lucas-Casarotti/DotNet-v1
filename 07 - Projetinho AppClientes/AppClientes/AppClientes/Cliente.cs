7a0d1784590a18c6739baba905e10ddf4f909a41
Lucas-Casarotti <casarotti@hotmail.com>
2025-02-26 15:27:05 -0300

    v8 - Projeto final

---
+++ 07 - Projetinho AppClientes/AppClientes/AppClientes/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cadastro
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public DateTime CadastradoEm { get; set; }
+    }
+}
